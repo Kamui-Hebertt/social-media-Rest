76acc5f1d3ddceab7005f86f1ce2e519ec485088
Kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-07-13 22:10:05 -0300

    feat:login authentication

--- routers/auth.js
+++ routers/auth.js
@@ -39,4 +39,30 @@ router.post('/register', async (req, res) => {
   // res.send("ok");
 });
 
+
+
+//Login 
+
+router.post("/login", async(req, res) => {
+ 
+
+ try{
+  const user = await User.findOne({email: req.body.email});
+   !user && res.status(404).json("user not found");
+
+   const validPassword = await bcrypt.compare(req.body.password, user.password);
+   
+   !validPassword && res.status(400).json("Invalid password");
+
+  
+   res.status(200).json(user)
+
+ 
+   } 
+  
+ catch(err){
+  console.log(err);
+ }
+})
+
 module.exports = router;
